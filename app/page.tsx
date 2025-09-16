ba2c9af5ee152c14703fa9171c519d7d94edd698
Gkfit2025 <promotiongkf46@gmail.com>
2025-09-16 10:28:49 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -494,7 +494,7 @@ export default function IVYHomePage() {
                 type: "Volunteer & Intern",
                 hostName: "Sunshine Children's Foundation",
                 hostImage: "/logo12.png",
-                image: "/child.png",
+                image: "/Child.png",
                 rating: 4.8,
                 reviews: 24,
                 price: "₹2,500/week",
